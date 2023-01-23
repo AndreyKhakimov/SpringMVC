9c7e5af369e6d194f8798cb6322d94de1c815eb4
AndreyKhakimov <khakimov013@gmail.com>
2023-01-23 22:26:11 +0100

    Spring version that fixes 500 error

--- src/main/java/ru/khakimov/springMVC/controllers/FirstController.java
+++ src/main/java/ru/khakimov/springMVC/controllers/FirstController.java
@@ -2,10 +2,8 @@ package ru.khakimov.springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
