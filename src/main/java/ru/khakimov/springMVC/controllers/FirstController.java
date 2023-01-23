f55b402f91db23e08afb143b327e128ea51d3aa5
AndreyKhakimov <khakimov013@gmail.com>
2023-01-23 23:47:41 +0100

    Add calculator feature

--- src/main/java/ru/khakimov/springMVC/controllers/FirstController.java
+++ src/main/java/ru/khakimov/springMVC/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.khakimov.springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,11 +10,45 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class FirstController {
 
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam(value = "a") int a,
+                            @RequestParam(value = "b") int b,
+                            @RequestParam(value = "action") String action,
+                            Model model
+    ) {
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", "" + result);
+
+        return "first/calculator";
+    }
+
+
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
+    public String helloPage(HttpServletRequest request, Model model) {
         String name = request.getParameter("name");
         String surname = request.getParameter("surname");
-        System.out.println("Hello " + name + " " + surname);
+
+        model.addAttribute("message", "Hello " + name + " " + surname);
+//        System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
 
