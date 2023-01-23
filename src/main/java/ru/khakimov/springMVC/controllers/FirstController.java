99282df871ea0cd5f2b349b5ac5e1e5c2523d79e
AndreyKhakimov <khakimov013@gmail.com>
2023-01-23 22:53:09 +0100

    Add RequestParam & HttpServletRequest in FirstController

--- src/main/java/ru/khakimov/springMVC/controllers/FirstController.java
+++ src/main/java/ru/khakimov/springMVC/controllers/FirstController.java
@@ -2,17 +2,24 @@ package ru.khakimov.springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        System.out.println("Hello " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodByePage() {
+    public String goodByePage(@RequestParam(value = "name", required = false) String name) {
+        System.out.println("Goodbye " + name);
         return "first/goodbye";
     }
 }
